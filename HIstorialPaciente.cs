b30d184c2480be9abc921fde14e42b87fffc9810
joshauto117@gmail.com <joshauto117@gmail.com>
2022-06-01 15:04:41 -0600

    clases

---
+++ HIstorialPaciente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyecto_final_233323
+{
+    public class HIstorialPaciente
+    {
+        public int IDConsult { get; set; }
+        public int NitPaciente { get; set; }
+        public int Fecha { get; set; }
+        public int grados   { get; set; }
+        public int presion { get; set; }
+        List<Agenda> sintomas { get; set; }
+        public string diagnodtico { get; set; }
+        public string tratamiento { get; set; }
+        public string Receta { get; set; }
+        public string Proximavisita { get; set; }
+        public int costoConsulta { get; set; }
+        List<HIstorialPaciente> imagenes { get; set; }
+    }
+}
---
+++ Historial de pacientes.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Historial de pacientes.aspx.cs" Inherits="proyecto_final_233323.Historial_de_pacientes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
